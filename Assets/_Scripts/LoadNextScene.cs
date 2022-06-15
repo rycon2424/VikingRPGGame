d61049343d1ba1b15609f91d15e466821eefd91d
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2022-06-15 15:11:18 +0200

    Quest 2 working

---
+++ Assets/_Scripts/LoadNextScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public void Z_ChangeScene(string scene)
+    {
+        SceneManager.LoadSceneAsync(scene);
+    }
+
+}
